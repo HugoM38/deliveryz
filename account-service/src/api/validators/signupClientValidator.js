eea56936903dea34079834581ab1957da4d0f989
Hugo <>
2024-04-22 16:19:39 +0200

    feat: add params to deliverer, client and cooker

--- account-service/src/api/validators/signupClientValidator.js
+++ account-service/src/api/validators/signupClientValidator.js
@@ -1,7 +1,12 @@
-const Joi = require('joi');
+const Joi = require('joi').extend(require('joi-phone-number'));
 
 const signupClientSchema = Joi.object({
-    username: Joi.string().alphanum().min(3).max(30).required(),
+    firstname: Joi.string().alphanum().required(),
+    lastname: Joi.string().alphanum().required(),
+    address: Joi.string().required(),
+    city: Joi.string().required(),
+    postalCode: Joi.string().alphanum().required(),
+    phoneNumber: Joi.string().phoneNumber().required(),
     email: Joi.string().email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } }).required(),
     password: Joi.string().pattern(new RegExp('^[a-zA-Z0-9]{3,30}$')).required(),
 });
