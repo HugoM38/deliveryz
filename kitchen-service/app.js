f5210c4c40ae3ca4158525796b5f0c9e3caea291
StrippoliJules <julesstrippoli@gmail.com>
2024-04-25 18:13:44 +0200

    feat: add cors to app.js kitchen and order

--- kitchen-service/app.js
+++ kitchen-service/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 require('dotenv').config();
 const accountRoutes = require('./src/api/routes/kitchenRoutes');
 
@@ -7,6 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
+app.use(cors({
+    origin: true, // Pour les tests de développement, permet à toutes les origines d'accéder à votre API
+}));
 app.use(bodyParser.json());
 app.use('/', accountRoutes);
 
--- order-service/app.js
+++ order-service/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 const orderRoutes = require('./src/api/routes/orderRoutes');
 const errorHandler = require('./src/api/middleware/errorHandler');
 
@@ -7,6 +8,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors({
+    origin: true, // Pour les tests de développement, permet à toutes les origines d'accéder à votre API
+}));
 app.use('/', orderRoutes);
 app.use(errorHandler);
 
