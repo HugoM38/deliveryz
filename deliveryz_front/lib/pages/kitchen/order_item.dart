5ed01d583701e5d9d4dd5de717d108ea31aaf469
Hugo <>
2024-04-26 15:28:52 +0200

    feat: change style of home cooker

--- deliveryz_front/lib/pages/kitchen/order_item.dart
+++ deliveryz_front/lib/pages/kitchen/order_item.dart
@@ -21,127 +21,140 @@ class OrderItemWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Expanded(
-          child: Text(
-            "Product: $product",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        Expanded(
-          child: Text(
-            "Price: $price",
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.onBackground,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-        ),
-        const SizedBox(width: 16),
-        if (status == "pending")
-          Row(
-            children: [
-            ElevatedButton(
-              onPressed: isEnabled ? () {
-                OrderService().cancelOrder(orderId).then((_) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor:
-                      Theme.of(context).colorScheme.secondary,
-                      content: Text('Item removed',
-                          style: TextStyle(
-                            color:
-                            Theme.of(context).colorScheme.onPrimary,
-                          )),
-                    ),
-                  );
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (BuildContext context) => const HomeCookerPage()),
-                  );
-                }).catchError((error) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor:
-                      Theme.of(context).colorScheme.error,
-                      content: Text(
-                        error.toString(),
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.onError),
-                      ),
-                    ),
-                  );
-                });
-              } : null,
-              child: Text('Validate',
-                  style: TextStyle(
-                      color: Theme
-                          .of(context)
-                          .colorScheme
-                          .onPrimary)
-              ),
-            ),
-            const SizedBox(width: 16),
-            ElevatedButton(
-              onPressed: isEnabled ? () {
-                OrderService().cancelOrder(orderId).then((_) { // TODO VALIDATE
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor:
-                      Theme.of(context).colorScheme.secondary,
-                      content: Text('Item removed',
-                          style: TextStyle(
-                            color:
-                            Theme.of(context).colorScheme.onPrimary,
-                          )),
-                    ),
-                  );
-                  Navigator.pushReplacement(
-                    context,
-                    MaterialPageRoute(builder: (BuildContext context) => const HomeCookerPage()),
-                  );
-                }).catchError((error) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    SnackBar(
-                      backgroundColor:
-                      Theme.of(context).colorScheme.error,
-                      content: Text(
-                        error.toString(),
-                        style: TextStyle(
-                            color: Theme.of(context).colorScheme.onError),
-                      ),
-                    ),
-                  );
-                });
-              } : null,
-              child: Text('Cancel',
-                  style: TextStyle(
-                      color: Theme
-                          .of(context)
-                          .colorScheme
-                          .onPrimary)
-                ),
-              ),
-            ]
-          )
-        else
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
           Expanded(
             child: Text(
-              "Status: $status",
+              "Product: $product",
               textAlign: TextAlign.left,
               style: TextStyle(
-                color: Theme.of(context).colorScheme.onBackground,
+                color: Theme.of(context).colorScheme.background,
                 fontWeight: FontWeight.bold,
               ),
             ),
           ),
-      ],
+          Expanded(
+            child: Text(
+              "Price: $price",
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.background,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          const SizedBox(width: 16),
+          if (status == "pending")
+            Row(
+              children: [
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.background,
+                  ),
+                ),
+                onPressed: isEnabled ? () {
+                  OrderService().cancelOrder(orderId).then((_) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                        content: Text('Item removed',
+                            style: TextStyle(
+                              color:
+                              Theme.of(context).colorScheme.onPrimary,
+                            )),
+                      ),
+                    );
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (BuildContext context) => const HomeCookerPage()),
+                    );
+                  }).catchError((error) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor:
+                        Theme.of(context).colorScheme.error,
+                        content: Text(
+                          error.toString(),
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onError),
+                        ),
+                      ),
+                    );
+                  });
+                } : null,
+                child: Text('Validate',
+                    style: TextStyle(
+                        color: Theme
+                            .of(context)
+                            .colorScheme
+                            .onBackground)
+                ),
+              ),
+              const SizedBox(width: 16),
+              ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    Theme.of(context).colorScheme.error,
+                  ),
+                ),
+                onPressed: isEnabled ? () {
+                  OrderService().cancelOrder(orderId).then((_) { // TODO VALIDATE
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor:
+                        Theme.of(context).colorScheme.secondary,
+                        content: Text('Item removed',
+                            style: TextStyle(
+                              color:
+                              Theme.of(context).colorScheme.onPrimary,
+                            )),
+                      ),
+                    );
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (BuildContext context) => const HomeCookerPage()),
+                    );
+                  }).catchError((error) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        backgroundColor:
+                        Theme.of(context).colorScheme.error,
+                        content: Text(
+                          error.toString(),
+                          style: TextStyle(
+                              color: Theme.of(context).colorScheme.onError),
+                        ),
+                      ),
+                    );
+                  });
+                } : null,
+                child: Text('Cancel',
+                    style: TextStyle(
+                        color: Theme
+                            .of(context)
+                            .colorScheme
+                            .onPrimary)
+                  ),
+                ),
+              ]
+            )
+          else
+            Expanded(
+              child: Text(
+                "Status: $status",
+                textAlign: TextAlign.left,
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.background,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+        ],
+      ),
     );
   }
 }
